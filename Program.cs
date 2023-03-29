9b2c9a0ce85f1645d99eaee9ec2ce906b5c81d7b
BhaveshKumarReddy <Admin@DESKTOP-HHR16QE>
2023-03-29 11:02:32 +0530

    second try to temp1

--- Program.cs
+++ Program.cs
@@ -2,23 +2,12 @@
 using CCollections;
 using System.Collections;
 using System.Collections.Generic;
-//gacutil /i
+
 namespace Collections
 {
-    // take input and print in file and screen
     public delegate int addDel(int a, int b);
-    //public dele
-    //gate float mulDel(int a, int b);
     class Program
     {
-        /*public int addDel(int a, int b)
-        {
-            return a + b;
-        }
-        public int mulDel(int a, int b)
-        {
-            return a * b;
-        }*/
         public static void callDes()
         {
             if (true) { Program p = new Program(); }
